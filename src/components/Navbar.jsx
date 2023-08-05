92d416b7917b00c1c601cc2b4dd22a6120f267c5
ribin <muhdribin@gmail.com>
2023-08-05 16:18:32 +0530

    complete authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  // console.log(user)
+  const navigate = useNavigate();
 
-  const {user,logOut}=UserAuth();
-  console.log(user)
+  const handlerLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="flex justify-between items-center p-4 z-[100] absolute w-full">
@@ -15,17 +24,29 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
-
-      <div>
-        <Link to="/login">
-          <button className="text-white  px-4 py-2 rounded">SIGN IN</button>
-        </Link>
-        <Link to="/signup">
-          <button className="text-white bg-red-600 px-4 py-2 rounded">
-            SIGN UP
-          </button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white  px-4 py-2 rounded">Account</button>
+          </Link>
+          <>
+            <button onClick={handlerLogout} className="text-white bg-red-600 px-4 py-2 rounded">
+              logout
+            </button>
+          </>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white  px-4 py-2 rounded">SIGN IN</button>
+          </Link>
+          <Link to="/signup">
+            <button  className="text-white bg-red-600 px-4 py-2 rounded">
+              SIGN UP
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       <div className="w-full h-screen">
@@ -15,8 +34,14 @@ const Login = () => {
           <div className="max-w-[450px] h-[500px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">SIGN IN</h1>
-              <form className="flex flex-col w-full h-full py-4" action="">
+              {error ? <p className="p-3 bg-red-400 mt-2">{error}</p> : null}
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col w-full h-full py-4"
+                action=""
+              >
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   className="p-3 my-2 bg-gray-600 opacity-[70%]  rounded"
                   type="email"
                   placeholder="Email"
@@ -24,6 +49,7 @@ const Login = () => {
                   autoComplete="email"
                 />
                 <input
+                  onChange={(e) => setPassword(e.target.value)}
                   className="p-3 my-2 bg-gray-600 opacity-[70%]  rounded"
                   type="password"
                   placeholder="Password"
@@ -41,9 +67,7 @@ const Login = () => {
                   <p>Need help?</p>
                 </div>
                 <p className="py-4">
-                  <span className="text-gray-600">
-                    New to netflix? 
-                  </span>
+                  <span className="text-gray-600">New to netflix?</span>
 
                   <Link to="/signup">
                     <span> signup</span>
@@ -56,6 +80,6 @@ const Login = () => {
       </div>
     </>
   );
-}
+};
 
-export default Login
+export default Login;
